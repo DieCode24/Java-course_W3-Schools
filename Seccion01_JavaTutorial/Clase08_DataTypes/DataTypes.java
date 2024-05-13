fb6023c97aaf0e23bd55d22d4dc81e5b3ea95410
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-13 04:02:29 -0500

    Edited option 2 DataTypes File

--- Seccion01_JavaTutorial/Clase08_DataTypes/DataTypes.java
+++ Seccion01_JavaTutorial/Clase08_DataTypes/DataTypes.java
@@ -35,7 +35,7 @@ public class DataTypes {
 
             // Estructura switch-case para manejar las diferentes opciones
             switch (opcion) {
-                case 1: // Opción para mostrar variables
+                case 1:
                     clearConsoleSafely(); // Limpiar la consola y manejar excepciones
                     System.out.println("Opción (" + opcion + ") | Tipos de datos primitivos");
                     divisorConsole(); // Imprimir un divisor
@@ -46,18 +46,70 @@ public class DataTypes {
                     clearConsoleSafely(); // Limpiar nuevamente antes de regresar al menú
                     break;
 
-                case 2: // Opción para limpiar la consola
+                case 2:
                     clearConsoleSafely(); // Limpiar la consola
+                    
                     System.out.println("Opción (" + opcion + ") | Instruccion opcion 2");
+                    
                     divisorConsole(); // Divisor visual
                     
-                    // Instruccion opcion 2
+                    // Aquí se explica la información sobre los tipos de datos primitivos en Java
+                    System.out.println("Números en Java");
+                    
+                    System.out.println("\nLos tipos de números primitivos se clasifican en dos grupos:");
+                    // Explicación sobre los tipos enteros y de punto flotante
+                    System.out.println("\n1) Tipos enteros => almacenan números enteros, positivos o negativos (como 123 o -456), sin decimales. Los tipos válidos son byte, short, int y long. El tipo que debes usar depende del valor numérico.");
+                    System.out.println("\n2)Tipos de punto flotante => representan números con una parte fraccionaria, que contienen uno o más decimales. Hay dos tipos: float y double.");
+
+                    // Descripción de los tipos enteros
+                    System.out.println("\nTipos enteros:");
+
+                    // Descripción y uso del tipo byte
+                    System.out.println("\tByte => El tipo de dato byte puede almacenar números enteros desde -128 hasta 127. Esto puede usarse en lugar de int u otros tipos enteros para ahorrar memoria cuando estás seguro de que el valor estará dentro de -128 y 127:");
+                    System.out.println("\n\t\t=> byte myNum = 100;");
+                    System.out.println("\t\t=> System.out.println(myNum);");
+
+                    // Descripción y uso del tipo short
+                    System.out.println("\n\n\tShort => El tipo de dato short puede almacenar números enteros desde -32768 hasta 32767:");
+                    System.out.println("\n\t\t=> short myNum = 5000;");
+                    System.out.println("\t\t=> System.out.println(myNum);");
+
+                    // Descripción y uso del tipo int
+                    System.out.println("\n\n\tInt => El tipo de dato int puede almacenar números enteros desde -2147483648 hasta 2147483647. En general, y en nuestro tutorial, el tipo de dato int es el preferido cuando creamos variables con un valor numérico.");
+                    System.out.println("\n\t\t=> int myNum = 100000;");
+                    System.out.println("\t\t=> System.out.println(myNum);");
+
+                    // Descripción y uso del tipo long
+                    System.out.println("\n\n\tLong => El tipo de dato long puede almacenar números enteros desde -9223372036854775808 hasta 9223372036854775807. Se utiliza cuando int no es lo suficientemente grande para almacenar el valor. Nota que debes terminar el valor con una \"L\":");
+                    System.out.println("\n\t\t=> long myNum = 15000000000L;");
+                    System.out.println("\t\t=> System.out.println(myNum);");
+
+                    // Descripción de los tipos de punto flotante
+                    System.out.println("\n\n\nTipos de punto flotante:");
+
+                    // Descripción y uso del tipo float
+                    System.out.println("\n\tFloat y Double => Debes usar un tipo de punto flotante siempre que necesites un número con un decimal, como 9.99 o 3.14515. Los tipos de datos float y double pueden almacenar números fraccionarios. Nota que debes terminar el valor con una \"f\" para floats y \"d\" para doubles:");
+                    System.out.println("\n\t\t=> float myNum = 5.75f;");
+                    System.out.println("\t\t=> System.out.println(myNum);");
+                    System.out.println("\t\t=> double myNum = 19.99d;");
+                    System.out.println("\t\t=> System.out.println(myNum);");
+
+                    // Descripción sobre cuál tipo de punto flotante usar
+                    System.out.println("\n\n¿Usar float o double?");
+                    System.out.println("\n\t=> La precisión de un valor de punto flotante indica cuántos dígitos puede tener el valor después del punto decimal. La precisión de float es solo de seis o siete dígitos decimales, mientras que las variables double tienen una precisión de aproximadamente 15 dígitos. Por lo tanto, es más seguro usar double para la mayoría de los cálculos.");
+
+                    // Descripción sobre números científicos
+                    System.out.println("\n\nNúmeros científicos => Un número de punto flotante también puede ser un número científico con una \"e\" para indicar la potencia de 10:");
+                    System.out.println("\n\t=> float f1 = 35e3f;");
+                    System.out.println("\t=> double d1 = 12E4d;");
+                    System.out.println("\t=> System.out.println(f1);");
+                    System.out.println("\t=> System.out.println(d1);");
 
                     pause(scanner); // Pausar para que el usuario vea el resultado
                     clearConsoleSafely(); // Limpiar nuevamente antes de regresar al menú
                     break;
 
-                case 3: // Opción para mostrar datos del estudiante
+                case 3:
                     clearConsoleSafely(); // Limpiar la consola
                     System.out.println("Opción (" + opcion + ") | Instruccion opcion 3");
                     divisorConsole(); // Divisor visual
@@ -68,7 +120,7 @@ public class DataTypes {
                     clearConsoleSafely(); // Limpiar nuevamente antes de regresar al menú
                     break;
 
-                case 4: // Opción para calcular el área de un rectángulo
+                case 4:
                     clearConsoleSafely(); // Limpiar la consola
                     System.out.println("Opción (" + opcion + ") | Instruccion opcion 4");
                     divisorConsole(); // Divisor visual
